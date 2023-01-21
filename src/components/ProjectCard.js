0ffd14eab1cb4b6a08cae8627bba293fa716334d
Jym Bocala <jym.bocala@gmail.com>
2023-01-22 01:47:34 +1100

    add project card styles and media queries

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,9 +5,11 @@ export default function ProjectCard(props) {
   return (
     <div className="project-card">
       <img src={props.img} className="project-card__img" alt="website screenshot" loading="lazy"/>
-      <h4>{props.title}</h4>
-      <p>{props.description}</p>
-      <button>Router Link</button>
+      <div className="project-card__content">
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+        <button>ROUTER LINK</button>
+      </div>
     </div>
   );
 }
