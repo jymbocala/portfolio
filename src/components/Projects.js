179230a08953b39f9da31abf807720e56005dd7e
Jym Bocala <jym.bocala@gmail.com>
2023-01-21 22:47:32 +1100

    create Projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <section className="projects" id="projects">
+      <div className="projects-inner-container">
+        <h2 className="projects-title">Projects</h2>
+        <h3>A selection of my work</h3>
+        {/* <ProjectCard /> */}
+      </div>
+    </section>
+  );
+}
---
+++ src/projectsData.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    id: 1,
+    title: "Quick Lesson Planner",
+    img: "",
+    description:
+      "Quick Lesson Planner is a web-based app that uses artificial intelligence to help teachers create lesson plans quickly and easily. With Quick Lesson Planner, teachers can save time and stress by letting the app handle the planning process.",
+  },
+  {
+    id: 2,
+    title: "Picsome Shop",
+    img: "",
+    description:
+      "Picsome Shop is a mock online shop using React. The app features a home page where users can browse and favorite products, and a cart page where they can manage the items they've added to their cart. I designed the app to be highly interactive and responsive, making it easy for users to navigate and interact with the various features. The end result is a seamless shopping experience that provides value to the users.",
+  },
+  {
+    id: 2,
+    title: "Portfolio Site",
+    img: "",
+    description: "",
+  },
+  {
+    id: 3,
+    title: "Tenzies Game",
+    img: "",
+    description:
+      "Tenzies is a popular dice game that encourages fast reactions and is super easy to learn. Roll until all dice are the same. Click each die to freeze it at its current value between rolls.",
+  },
+];
