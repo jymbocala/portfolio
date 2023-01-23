82c86975391c7e59eb33dc553d735f02f4a739ee
Jym Bocala <jym.bocala@gmail.com>
2023-01-23 14:31:37 +1100

    add contacts elements

--- src/App.js
+++ src/App.js
@@ -19,6 +19,17 @@ function App() {
         <Hero />
         <Projects />
         <AboutMe />
+        <section id="contact" class="contact sec-pad">
+          <div class="contacts-container">
+            <div class="heading-sec">
+              <h2 class="heading-sec__main">Contact</h2>
+              <p class="heading-sec__sub">
+                Please reach out if you have any questions! Send me an email at
+                jym.bocala@gmail.com or call me directly on (+61) 422 672 865.
+              </p>
+            </div>
+          </div>
+        </section>
       </main>
 
       <Footer />
