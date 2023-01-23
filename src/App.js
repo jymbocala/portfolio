d59f0fb7453410050e3e6d3b23d36914bfd7ccc6
Jym Bocala <jym.bocala@gmail.com>
2023-01-23 21:48:07 +1100

    add Contact component and content

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import AboutMe from "./components/AboutMe";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
@@ -17,17 +18,7 @@ function App() {
         <Hero />
         <Projects />
         <AboutMe />
-        <section id="contact" className="contact sec-pad">
-          <div className="contacts-container">
-            <div className="heading-sec">
-              <h2 className="heading-sec__main heading">Contact</h2>
-              <p className="heading-sec__sub">
-                Please reach out if you have any questions! Send me an email at
-                jym.bocala@gmail.com or call me directly on (+61) 422 672 865.
-              </p>
-            </div>
-          </div>
-        </section>
+        <Contact />
       </main>
 
       <Footer />
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section id="contact" className="contact sec-pad">
+      <div className="contacts-container">
+        <div className="heading-sec">
+          <h2 className="heading-sec__main heading">Contact</h2>
+          <p className="heading-sec__sub">
+            Please reach out if you have any questions! Send me an email at
+            jym.bocala@gmail.com or call me directly on (+61) 422 672 865.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
